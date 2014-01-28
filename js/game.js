73dd90877b1288731a2056090906294338c7d1c9
mirage <mirage@panda-ubuntu.(none)>
2014-01-29 00:47:29 +0400

    анимировал пятнашки и заставил их двигаться

--- js/game.js
+++ js/game.js
@@ -5,4 +5,9 @@ var gameApp = angular.module('gameApp', []);
 gameApp.controller('pyatnashkyCtrl', function ($scope) {
 	var area = new gameArea();
 	$scope.Places = area.Places;
+
+	$scope.move = function(number) {
+		area.move(number);
+		$scope.Places = area.Places;
+	}
 });
